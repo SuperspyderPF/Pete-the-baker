a4be8157d67301d61e84cf33a89ef4533a6e96ad
daniel asherov <djasherov@gmail.com>
2022-08-10 08:45:30 -0800

    first commit

---
+++ Pete, The Baker.js
@@ -0,0 +1,6 @@
+//interesting solution 
+//https://www.codewars.com/kata/525c65e51bf619685c000059
+
+const cakes = (needs, has) => Math.min(
+    ...Object.keys(needs).map(key => Math.floor(has[key] / needs[key] || 0))
+  )
